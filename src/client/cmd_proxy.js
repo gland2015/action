947cdd560b7c1b08e49142c4299f1497ef006c50
gland <1147940842@qq.com>
2021-10-30 20:12:40 +0800

    66d

--- src/client/cmd_proxy.js
+++ src/client/cmd_proxy.js
@@ -10,146 +10,138 @@ import { Shell } from "../public/Shell.js";
 export class Proxy {
   constructor() {
     this.shell = new Shell();
+
+    this.PORT = process.env.PORT || 8081;
+    this.SSL_CA_DIR = path.resolve(SSL_CERT_ROOT_PATH, "./.http-mitm-proxy");
+    this.PATH_CA_PEM = path.resolve(SSL_CERT_ROOT_PATH, "RootCA.pem");
+    this.PATH_CA_KEY = path.resolve(SSL_CERT_ROOT_PATH, "RootCA.key");
+  }
+
+  async getFileMapPath(filepath) {
+    let result;
+    if (filepath) {
+      if (path.isAbsolute(filepath)) {
+        result = filepath;
+      } else {
+        result = path.resolve(process.cwd(), filepath);
+      }
+      let statfile = await fs.stat(result);
+      if (!statfile.isFile()) {
+        result = path.resolve(result, "fileMap.json");
+      }
+    } else {
+      result = path.resolve(process.cwd(), "./fileMap.json");
+    }
+    return result;
+  }
+
+  parseHostname(hostname) {
+    const domainInfo = parseDomain(hostname);
+    if (domainInfo.type === ParseResultType.Listed) {
+      const baseDomain = [
+        ...domainInfo.subDomains.slice(1),
+        domainInfo.domain,
+        ...domainInfo.topLevelDomains,
+      ].join(".");
+
+      return {
+        basename: "-." + hostname,
+        hosts: ["*." + baseDomain, baseDomain],
+      };
+    } else if (domainInfo.type == ParseResultType.Ip) {
+      return {
+        basename: hostname,
+        ips: [hostname],
+      };
+    } else {
+      return {
+        basename: hostname,
+        hosts: [hostname],
+      };
+    }
   }
 
   async exec(args) {
-    const port = process.env.PORT || 8081;
-    const sslCaDir = path.resolve(SSL_CERT_ROOT_PATH, "./.http-mitm-proxy");
-
-    let filemapfile;
-    let filemapdir;
-    if (args.map) {
-      if (path.isAbsolute(args.map)) {
-        filemapfile = args.map;
-      } else {
-        filemapfile = path.resolve(process.cwd(), args.map);
-      }
-      let isExist = await fs.pathExists(filemapfile);
-      if (!isExist) {
-        throw new Error("path is not exist: " + filemapfile);
-      }
-      let statfile = await fs.stat(filemapfile);
-      if (statfile.isDirectory()) {
-        filemapfile = path.resolve(filemapfile, "fileMap.json");
-      }
-    } else {
-      filemapfile = path.resolve(process.cwd(), "./fileMap.json");
-    }
-    filemapdir = path.dirname(filemapfile);
+    const PATH_fileMap = this.getFileMapPath(args.map);
+    const PATH_fileMapDir = path.dirname(PATH_fileMap);
 
     let fileMap;
     try {
-      fileMap = await fs.readFile(filemapfile);
+      fileMap = await fs.readFile(PATH_fileMap);
       fileMap = JSON.parse(fileMap);
     } catch (err) {
-      console.log("file is not right: " + filemapfile, err);
+      console.log("file is not right: " + PATH_fileMap, err);
       return;
     }
 
     const http_mitm_proxy = HttpMitmProxy();
-
     http_mitm_proxy.onError(function (ctx, err, errorKind) {});
     http_mitm_proxy.onRequest((ctx, callback) => {
-      const request = ctx.clientToProxyRequest;
-      const url =
-        (ctx.isSSL ? "https://" : "http://") +
-        request.headers.host +
-        request.url;
-      const needProxy = !!fileMap[url];
-      console.log("------url--------" + url + "------url--------", needProxy);
-      if (needProxy) {
+      const req = ctx.clientToProxyRequest;
+      const url = `http${ctx.isSSL ? "s" : ""}://` + req.headers.host + req.url;
+
+      const proxyFile = fileMap[url];
+
+      console.log("------url-------" + url + "------url------", !!proxyFile);
+
+      if (proxyFile) {
         let filePath;
-        if (path.isAbsolute(fileMap[url])) {
-          filePath = fileMap[url];
+        if (path.isAbsolute(proxyFile)) {
+          filePath = proxyFile;
         } else {
-          filePath = path.join(filemapdir, fileMap[url]);
-          const response = ctx.proxyToClientResponse;
-          const file = send(request, encodeURI(filePath), {});
-          file.pipe(response);
-          return;
+          filePath = path.join(PATH_fileMapDir, proxyFile);
         }
+        const res = ctx.proxyToClientResponse;
+        const file = send(req, encodeURI(filePath), {});
+        file.pipe(res);
+        return;
       }
       callback();
     });
 
     http_mitm_proxy.onCertificateRequired = (hostname, callback) => {
-      const result = {};
+      const { hosts, basename } = this.parseHostname(hostname);
 
-      let basename = hostname;
-      const domainInfo = parseDomain(hostname);
-      if (domainInfo.type === ParseResultType.Listed) {
-        hostname = [
-          ...domainInfo.subDomains.slice(1),
-          domainInfo.domain,
-          ...domainInfo.topLevelDomains,
-        ].join(".");
-        result.hosts = ["*." + hostname, hostname];
-        basename = "-." + hostname;
-      }
-
-      result.keyFile = path.resolve(
-        SSL_CERT_ROOT_PATH,
-        `sub_cert_${basename}/${basename}.key`
-      );
-      result.certFile = path.resolve(
-        SSL_CERT_ROOT_PATH,
-        `sub_cert_${basename}/${basename}.crt`
-      );
+      const result = {
+        hosts,
+        keyFile: path.resolve(
+          SSL_CERT_ROOT_PATH,
+          `sub_cert_${basename}/${basename}.key`
+        ),
+        certFile: path.resolve(
+          SSL_CERT_ROOT_PATH,
+          `sub_cert_${basename}/${basename}.crt`
+        ),
+      };
 
       return callback(null, result);
     };
 
-    const ca_pem_path = path.resolve(SSL_CERT_ROOT_PATH, "RootCA.pem");
-    const ca_key_path = path.resolve(SSL_CERT_ROOT_PATH, "RootCA.key");
     http_mitm_proxy.onCertificateMissing = async (ctx, files, callback) => {
-      // console.log('Looking for "%s" certificates', ctx.hostname);
-      // console.log('"%s" missing', ctx.files.keyFile);
-      // console.log('"%s" missing', ctx.files.certFile);
-      const result = {};
+      const { hosts, ips, basename } = this.parseHostname(hostname);
 
-      const domains = [];
-      const ips = [];
-
-      let hostname = ctx.hostname;
-      let basename = hostname;
-      const domainInfo = parseDomain(hostname);
-      if (domainInfo.type === ParseResultType.Listed) {
-        hostname = [
-          ...domainInfo.subDomains.slice(1),
-          domainInfo.domain,
-          ...domainInfo.topLevelDomains,
-        ].join(".");
-        domains.push("*." + hostname, hostname);
-        result.hosts = ["*." + hostname, hostname];
-        basename = "-." + hostname;
-      } else if (domainInfo.type === ParseResultType.Ip) {
-        ips.push(hostname);
-      } else {
-        domains.push(hostname);
-      }
-
-      let outDir = path.dirname(ctx.files.keyFile);
-
+      const outDir = path.dirname(ctx.files.keyFile);
       await this.shell.GenSSLSubCert(
-        domains,
+        hosts,
         ips,
         outDir,
         basename,
-        ca_pem_path,
-        ca_key_path
+        this.PATH_CA_PEM,
+        this.PATH_CA_KEY
       );
 
-      result.keyFileData = (await fs.readFile(ctx.files.keyFile)).toString();
-      result.certFileData = (await fs.readFile(ctx.files.certFile)).toString();
-
+      const result = {
+        keyFileData: (await fs.readFile(ctx.files.keyFile)).toString(),
+        certFileData: (await fs.readFile(ctx.files.certFile)).toString(),
+      };
       return callback(null, result);
     };
 
     return new Promise((resolve, reject) => {
       http_mitm_proxy.listen(
         {
-          port,
-          sslCaDir,
+          port: this.PORT,
+          sslCaDir: this.SSL_CA_DIR,
         },
         function () {
           console.log("port", port);
